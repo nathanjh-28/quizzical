826f1fc404074891e2e56e72051de63661607e87
nathanjh-28 <nathan@nathanjharris.com>
2022-07-14 11:54:01 -0500

    quizzical app started with create react app and tested api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const apiURL = 'https://opentdb.com/api.php?amount=5&category=27&difficulty=medium&type=multiple'
 
-function App() {
+export default function App() {
+
+
+  React.useEffect(() => {
+    async function getQuestions() {
+      const res = await fetch(apiURL)
+      const data = await res.json()
+      console.log(data)
+    }
+    getQuestions()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Quizzical</h1>
   );
 }
 
-export default App;
