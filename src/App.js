c64f4f27f37d27f1de79248cea81d65d992ad5f4
nathanjh-28 <nathan@nathanjharris.com>
2022-07-14 12:38:55 -0500

    general markup complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
-const apiURL = 'https://opentdb.com/api.php?amount=5&category=27&difficulty=medium&type=multiple'
+import Question from "./components/Question";
+import SubmitBtn from './components/SubmitBtn'
+
+
+const apiURL = 'https://opentdb.com/api.php?amount=5&category=27&difficulty=easy&type=multiple'
 
 export default function App() {
 
@@ -8,13 +12,22 @@ export default function App() {
     async function getQuestions() {
       const res = await fetch(apiURL)
       const data = await res.json()
-      console.log(data)
+      console.log(data.results)
     }
     getQuestions()
   }, [])
 
   return (
-    <h1>Quizzical</h1>
+    <main>
+      <h1>Quizzical</h1>
+      <p>5 random questions from Open Trivia Database</p>
+      <button>Start Quiz</button>
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+      <SubmitBtn />
+    </main>
   );
 }
 
---
+++ src/components/Question.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Question(props) {
+    return (<div>
+        <h2> This is a question</h2>
+        <ul>
+            <li>answer 1</li>
+            <li>answer 2</li>
+            <li>answer 3</li>
+            <li>answer 4</li>
+        </ul>
+        <hr />
+
+    </div>)
+}
