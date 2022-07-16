2ab1d2a2962d2dd0dac0143937af0e5c663280b1
nathanjh-28 <nathan@nathanjharris.com>
2022-07-16 15:47:42 -0500

    randomize multiple choice answers and render them complete

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ const apiURL = 'https://opentdb.com/api.php?amount=5&category=27&difficulty=easy
 
 export default function App() {
 
+  function swap(arr, idx1, idx2) {
+    let temp = arr[idx1];
+    arr[idx1] = arr[idx2];
+    arr[idx2] = temp;
+    return arr;
+  }
+
+  function shuffle(arr) {
+    let count = 5;
+    while (count > 0) {
+      swap(arr, Math.floor(Math.random() * arr.length), Math.floor(Math.random() * arr.length))
+      count--;
+    }
+    return arr;
+  }
+
+
 
   const [questions, setQuestions] = React.useState([]);
 
@@ -23,7 +40,7 @@ export default function App() {
 
   const questionElements = questions.map((obj, idx) => {
     return <Question
-      question={obj.question}
+      obj={obj}
       key={idx} />
   })
 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,24 +1,46 @@
 import React from "react";
 
-export default function Question(props) {
+export default function Question({ obj }) {
+
+
+    function swap(arr, idx1, idx2) {
+        let temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+        return arr;
+    }
+
+    function shuffle(arr) {
+        let count = 5;
+        while (count > 0) {
+            swap(arr, Math.floor(Math.random() * arr.length), Math.floor(Math.random() * arr.length))
+            count--;
+        }
+        return arr;
+    }
+
+    const answerArr = shuffle([...obj.incorrect_answers, obj.correct_answer])
+
+
     return (<div>
-        <h2> {props.question}</h2>
+        <h2> {obj.question}</h2>
         <label>
-            A.
-            <input type="radio" name={props.question} id={props.question} />
+            <input type="radio" name={obj.question} id={obj.question} />
+            {answerArr[0]}
         </label>
         <label>
-            B.
-            <input type="radio" name={props.question} id={props.question} />
+            <input type="radio" name={obj.question} id={obj.question} />
+            {answerArr[1]}
         </label>
         <label>
-            C.
-            <input type="radio" name={props.question} id={props.question} />
+            <input type="radio" name={obj.question} id={obj.question} />
+            {answerArr[2]}
         </label>
         <label>
-            D.
-            <input type="radio" name={props.question} id={props.question} />
+            <input type="radio" name={obj.question} id={obj.question} />
+            {answerArr[3]}
         </label>
+
 
         <hr />
 
