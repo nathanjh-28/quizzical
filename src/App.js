de07d71ecfdb09c8340453a2b4ba9f35a6e833b3
nathanjh-28 <nathan@nathanjharris.com>
2022-07-17 23:10:37 -0500

    got answers state up and working and discovered a new bug

--- src/App.js
+++ src/App.js
@@ -8,32 +8,24 @@ const apiURL = 'https://opentdb.com/api.php?amount=5&category=27&difficulty=easy
 
 export default function App() {
 
-  function swap(arr, idx1, idx2) {
-    let temp = arr[idx1];
-    arr[idx1] = arr[idx2];
-    arr[idx2] = temp;
-    return arr;
-  }
-
-  function shuffle(arr) {
-    let count = 5;
-    while (count > 0) {
-      swap(arr, Math.floor(Math.random() * arr.length), Math.floor(Math.random() * arr.length))
-      count--;
-    }
-    return arr;
-  }
-
-
-
   const [questions, setQuestions] = React.useState([]);
+
+  const [answers, setAnswers] = React.useState([])
+
+  function handleAnswers(ans, idx) {
+    setAnswers(prevAns => {
+      const newAns = [...prevAns]
+      newAns[idx] = ans;
+      return newAns
+    })
+  }
 
   React.useEffect(() => {
     async function getQuestions() {
       const res = await fetch(apiURL)
       const data = await res.json()
       setQuestions(data.results)
-      console.log(data.results)
+      //console.log(data.results)
     }
     getQuestions()
   }, [])
@@ -41,8 +33,13 @@ export default function App() {
   const questionElements = questions.map((obj, idx) => {
     return <Question
       obj={obj}
-      key={idx} />
+      key={idx}
+      idx={idx}
+      handleChange={handleAnswers}
+    />
   })
+
+  console.log(answers)
 
   return (
     <main>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-export default function Question({ obj }) {
+export default function Question({ obj, handleChange, idx }) {
 
-
+    //helper
     function swap(arr, idx1, idx2) {
         let temp = arr[idx1];
         arr[idx1] = arr[idx2];
         arr[idx2] = temp;
         return arr;
     }
-
+    // helper
     function shuffle(arr) {
         let count = 5;
         while (count > 0) {
@@ -21,27 +21,23 @@ export default function Question({ obj }) {
 
     const answerArr = shuffle([...obj.incorrect_answers, obj.correct_answer])
 
+    const answerArrEls = answerArr.map(item => {
+        return (<label>
+            <input type="radio"
+                name={obj.question}
+                id={obj.question}
+                value={item}
+                onChange={() => handleChange(item, idx)}
+
+            />
+            {item}
+        </label>)
+    })
+
 
     return (<div>
         <h2> {obj.question}</h2>
-        <label>
-            <input type="radio" name={obj.question} id={obj.question} />
-            {answerArr[0]}
-        </label>
-        <label>
-            <input type="radio" name={obj.question} id={obj.question} />
-            {answerArr[1]}
-        </label>
-        <label>
-            <input type="radio" name={obj.question} id={obj.question} />
-            {answerArr[2]}
-        </label>
-        <label>
-            <input type="radio" name={obj.question} id={obj.question} />
-            {answerArr[3]}
-        </label>
-
-
+        {answerArrEls}
         <hr />
 
     </div>)
